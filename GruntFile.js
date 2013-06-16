a6d99c0321e0ab5635a67db8149d0b0c73296d27
Richard Willis <willis.rh@gmail.com>
2013-06-16 11:55:05 +0100

    More grunt tasks

--- GruntFile.js
+++ GruntFile.js
@@ -1,5 +1,6 @@
 module.exports = function(grunt) {
 
+  /* Set the config */
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     jshint: {
@@ -79,46 +80,69 @@ module.exports = function(grunt) {
       }
     },
     modernizr: {
-     devFile: "src/components/modernizr/modernizr.js",
-     outputFile: "public/js/vendor/modernizr.js",
-     // Based on default settings on http://modernizr.com/download/
-     extra: {
-       shiv: true,
-       printshiv: false,
-       load: true,
-       mq: false,
-       cssclasses: true
-     },
-     extensibility: {
-       addtest: false,
-       prefixed: false,
-       teststyles: false,
-       testprops: false,
-       testallprops: false,
-       hasevents: false,
-       prefixes: false,
-       domprefixes: false
-     },
-     uglify: true,
-     parseFiles: true,
-     files: ['src/js']
-   }
+      devFile: "src/components/modernizr/modernizr.js",
+      outputFile: "public/js/vendor/modernizr.js",
+      extra: {
+        shiv: false,
+        printshiv: false,
+        load: false,
+        mq: false,
+        cssclasses: true
+      },
+      extensibility: {
+        addtest: false,
+        prefixed: false,
+        teststyles: false,
+        testprops: false,
+        testallprops: false,
+        hasevents: false,
+        prefixes: false,
+        domprefixes: false
+      },
+      uglify: true,
+      parseFiles: true,
+      files: ['src/js']
+    },
+    styleguide: {
+      dist: {
+          name: 'Style Guide',
+          framework: 'kss-node',
+          files: {
+            'docs/css/core': 'src/sass/core/*.scss',
+            'docs/css/components': 'src/sass/components/*.scss',
+          }
+      }
+    },
+    connect: {
+      docs: {
+        options: {
+          port: 9001,
+          base: 'docs',
+          keepalive: true
+        }
+      }
+    }
   });
 
-  // Load the tasks
+  /* Load the tasks */
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-jasmine');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-contrib-connect');
   grunt.loadNpmTasks('grunt-jsdoc');
   grunt.loadNpmTasks("grunt-modernizr");
+  grunt.loadNpmTasks('grunt-styleguide');
 
-  // Register custom tasks
-  grunt.registerTask('lint', ['jshint']);
-  grunt.registerTask('test', ['lint', 'jasmine']);
-  grunt.registerTask('docs', ['clean', 'jsdoc']);
-  grunt.registerTask('build', ['test', 'compass:dev', 'modernizr']);
-  grunt.registerTask('release', ['test', 'docs', 'compass:dist', 'modernizr']);
-  grunt.registerTask('default', ['build']);
+  /* Register custom tasks */
+  grunt.registerTask('viewdocs', 'Spins up a connect server to display the API documentation', function() {
+    grunt.task.run('connect:docs');
+  });
+  grunt.registerTask('lint', 'Checks code for syntax errors.', ['jshint']);
+  grunt.registerTask('test', 'Runs all the tests.', ['lint', 'jasmine']);
+  grunt.registerTask('docs', 'Generates the API documentation.', ['clean', 'jsdoc', 'styleguide:dist']);
+  grunt.registerTask('build', 'The development build task.', ['modernizr', 'test', 'compass:dev']);
+  grunt.registerTask('release', 'The production build task.', ['modernizr', 'test', 'compass:dist', 'docs']);
+  grunt.registerTask('default', 'Our default task for running `grunt`.', ['build']);
 };
